9ccf503379188e4188019d5f11610298077d7900
anthonyOviedo <antony-08@hotmail.com>
2026-07-29 19:06:20 -0600

    first clean commit

--- server/core/db.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from 'pg';
-import { env } from '../config/env';
-
-export const pool = new Pool({ connectionString: env.DATABASE_URL });
-
-export async function query<T = any>(sql: string, params?: unknown[]): Promise<T[]> {
-  const result = await pool.query(sql, params as any[]);
-  return result.rows as T[];
-}
